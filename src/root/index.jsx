aea5ea37e9d8a574ba1cd76d397a10644e6b5094
redmiRX <redmirx9@gmail.com>
2022-11-06 18:31:28 +0500

    project

---
+++ src/root/index.jsx
@@ -0,0 +1,16 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+function Root() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/home" element={<h1>Home</h1>} />
+        <Route path="/properties" element={<h1>Properties</h1>} />
+        <Route path="*" element={<h1>404 NOT FOUND</h1>} />
+        <Route path="/" Navigate />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default Root;
