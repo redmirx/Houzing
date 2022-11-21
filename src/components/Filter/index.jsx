985748bf201e42f8a75532df65269b6d6a0e38f2
redmirx <redmirx9@gmail.com>
2022-11-21 23:36:21 +0500

    filter

--- src/components/Filter/index.jsx
+++ src/components/Filter/index.jsx
@@ -2,8 +2,13 @@ import React, { useRef } from "react";
 import { Container, Icons, MenuWrapper, Section } from "./style";
 import { Input, Button } from "../Generics";
 import { Dropdown } from "antd";
+import UseReplace from "./../../hooks/UseReplace";
+import { useNavigate, useLocation } from "react-router-dom";
 
 const Filter = () => {
+  const navigate = useNavigate();
+  const location = useLocation();
+
   const countryRef = useRef();
   const regionRef = useRef();
   const cityRef = useRef();
@@ -14,26 +19,75 @@ const Filter = () => {
   const minPriceRef = useRef();
   const maxPriceRef = useRef();
 
+  const onChange = ({ target: { name, value } }) => {
+    navigate(`${location?.pathname}${UseReplace(name, value)}`);
+  };
+
   const menu = () => {
     return (
       <MenuWrapper>
         <h1 className="subTitle">Address</h1>
         <Section>
-          <Input ref={countryRef} placeholder="Country" />
-          <Input ref={regionRef} placeholder="Region" />
-          <Input ref={cityRef} placeholder="City" />
-          <Input ref={zipCodeRef} placeholder="Zip code" />
+          <Input
+            onChange={onChange}
+            name="country"
+            ref={countryRef}
+            placeholder="Country"
+          />
+          <Input
+            onChange={onChange}
+            name="region"
+            ref={regionRef}
+            placeholder="Region"
+          />
+          <Input
+            onChange={onChange}
+            name="city"
+            ref={cityRef}
+            placeholder="City"
+          />
+          <Input
+            onChange={onChange}
+            name="zip_code"
+            ref={zipCodeRef}
+            placeholder="Zip code"
+          />
         </Section>
         <h1 className="subTitle">Apartment info</h1>
         <Section>
-          <Input ref={roomsRef} placeholder="Rooms" />
-          <Input ref={sizeRef} placeholder="Size" />
-          <Input ref={sortRef} placeholder="Sort" />
+          <Input
+            onChange={onChange}
+            ref={roomsRef}
+            name="room"
+            placeholder="Rooms"
+          />
+          <Input
+            onChange={onChange}
+            ref={sizeRef}
+            name="size"
+            placeholder="Size"
+          />
+          <Input
+            onChange={onChange}
+            ref={sortRef}
+            name="sort"
+            placeholder="Sort"
+          />
         </Section>
         <h1 className="subTitle">Price</h1>
         <Section>
-          <Input ref={minPriceRef} placeholder="Min price" />
-          <Input ref={maxPriceRef} placeholder="Max price" />
+          <Input
+            onChange={onChange}
+            ref={minPriceRef}
+            name="min_price"
+            placeholder="Min price"
+          />
+          <Input
+            onChange={onChange}
+            ref={maxPriceRef}
+            name="max_price"
+            placeholder="Max price"
+          />
         </Section>
 
         {/* <h1 className="subTitle">Footer</h1>
---
+++ src/hooks/UseReplace.jsx
@@ -0,0 +1,10 @@
+const UseReplace = (query, value) => {
+  const url = new URL(window.location.href);
+  url.searchParams.set(query, value);
+  if (!value && value !== 0) {
+    url.searchParams.delete(query);
+  }
+  return url.search;
+};
+
+export default UseReplace;
