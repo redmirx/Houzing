126e62f903cbd1ee7c2777204d8381b0bd74b400
redmirx <redmirx9@gmail.com>
2022-12-03 04:56:06 +0500

    home page

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,6 +1,5 @@
-{
-  /* eslint-disable-next-line jsx-a11y/anchor-is-valid */
-}
+/* eslint-disable-next-line jsx-a11y/anchor-is-valid */
+
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import {
